f50e4442bebfb5f7482a47d28a709cb83fca2b64
Chilling Coder <101239940+chill-c0der@users.noreply.github.com>
2022-03-19 05:05:18 +0000

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-sudo apt update
-sudo apt upgrade
-sudo apt install menu
-sudo apt install figlet
-sudo apt install neofetch
-sudo apt install lolcat
-sudo apt install git
-sudo bash geekstall.sh | lolcat
